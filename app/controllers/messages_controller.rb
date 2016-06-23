de2fcbca724e6983d2db3860b9bda86050bae642
Masaki <testmn777@gmail.com>
2016-06-23 08:45:58 +0000

    create message model

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,16 +4,16 @@ class MessagesController < ApplicationController
     # Messageを全て取得する。
     @messages = Message.all
   end
-  
+
   def create
     @message = Message.new(message_params)
     @message.save
     redirect_to root_path , notice: 'メッセージを保存しました'
   end
-  
-private
-def message_params
-params.require(:message).permit(:name, :body)
-end
 
+  private
+  def message_params
+    params.require(:message).permit(:name, :body)
+  end
+  ## ここまで
 end
